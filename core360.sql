6ccd5c87d9b104c7924e14fd0a10bf0b8550f0e4
Shayban Khan <shaybankhan12345@gmail.com>
2024-11-11 14:45:37 +0530

    added

--- core360.sql
+++ core360.sql
@@ -1,4 +1,5 @@
-use core360;
+CREATE DATABASE  IF NOT EXISTS `core360` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `core360`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: core360
@@ -15,6 +16,33 @@ use core360;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `categoryid` varchar(50) NOT NULL,
+  `categoryname` varchar(50) DEFAULT NULL,
+  `orgid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`categoryid`),
+  KEY `orgid` (`orgid`),
+  CONSTRAINT `category_ibfk_1` FOREIGN KEY (`orgid`) REFERENCES `organization` (`orgid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES ('2CWSX5V4Hg','Tank','eGpZhbaA38'),('4IWZjKIRd2','Furniture','eGpZhbaA38'),('nEnGMmBT2h','Test','eGpZhbaA38'),('orRmyEbgKk','human','eGpZhbaA38');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `inventory`
@@ -43,7 +71,38 @@ CREATE TABLE `inventory` (
 
 LOCK TABLES `inventory` WRITE;
 /*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+INSERT INTO `inventory` VALUES ('aDdZL2CHVm','Furniture','Table',2000,'Leather Tables','20','eGpZhbaA38'),('pjXZCIgqTx','Tank','Fish Tank',780,'Plastic fish tank','200','eGpZhbaA38');
 /*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marketing`
+--
+
+DROP TABLE IF EXISTS `marketing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `marketing` (
+  `marketing_id` varchar(50) NOT NULL,
+  `campaign_name` varchar(50) DEFAULT NULL,
+  `clicks` bigint(20) DEFAULT NULL,
+  `conversion` bigint(20) DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  `orgid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`marketing_id`),
+  KEY `orgid` (`orgid`),
+  CONSTRAINT `marketing_ibfk_1` FOREIGN KEY (`orgid`) REFERENCES `organization` (`orgid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marketing`
+--
+
+LOCK TABLES `marketing` WRITE;
+/*!40000 ALTER TABLE `marketing` DISABLE KEYS */;
+INSERT INTO `marketing` VALUES ('KjT8ldODnb','social media',5,1,'2024-11-11','eGpZhbaA38'),('XGxNd0Mj3n','Fish Tank ',0,0,'2024-11-11','eGpZhbaA38'),('YW9WnKyRmz','Wooden table Marketing',2,2,'2024-11-11','eGpZhbaA38');
+/*!40000 ALTER TABLE `marketing` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -92,7 +151,7 @@ CREATE TABLE `otp` (
 
 LOCK TABLES `otp` WRITE;
 /*!40000 ALTER TABLE `otp` DISABLE KEYS */;
-INSERT INTO `otp` VALUES ('sehrozkhan2704@gmail.com','780004'),('sehrozkhan2704@gmail.com','994385'),('sehrozkhan2704@gmail.com','823285'),('sehrozkhan2704@gmail.com','138244'),('sehrozkhan2704@gmail.com','580196'),('sehrozkhan2704@gmail.com','918532'),('sehrozkhan2704@gmail.com','820304'),('sehrozkhan2704@gmail.com','311486'),('sehrozkhan2704@gmail.com','230422'),('sehrozkhan2704@gmail.com','596455'),('sehrozkhan2704@gmail.com','888330'),('sehrozkhan2704@gmail.com','693400'),('sehrozkhan2704@gmail.com','859223'),('sehrozkhan2704@gmail.com','549643'),('sehrozkhan2704@gmail.com','144910'),('sehrozkhan2704@gmail.com','770387'),('sehrozkhan2704@gmail.com','870415'),('yobegey752@anypng.com','732128');
+INSERT INTO `otp` VALUES ('sehrozkhan2704@gmail.com','780004'),('sehrozkhan2704@gmail.com','994385'),('sehrozkhan2704@gmail.com','823285'),('sehrozkhan2704@gmail.com','138244'),('sehrozkhan2704@gmail.com','580196'),('sehrozkhan2704@gmail.com','918532'),('sehrozkhan2704@gmail.com','820304'),('sehrozkhan2704@gmail.com','311486'),('sehrozkhan2704@gmail.com','230422'),('sehrozkhan2704@gmail.com','596455'),('sehrozkhan2704@gmail.com','888330'),('sehrozkhan2704@gmail.com','693400'),('sehrozkhan2704@gmail.com','859223'),('sehrozkhan2704@gmail.com','549643'),('sehrozkhan2704@gmail.com','144910'),('sehrozkhan2704@gmail.com','770387'),('sehrozkhan2704@gmail.com','870415'),('yobegey752@anypng.com','732128'),('jibale6332@edectus.com','454553');
 /*!40000 ALTER TABLE `otp` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,11 +193,12 @@ CREATE TABLE `task` (
   `assignedat` varchar(50) DEFAULT NULL,
   `deadline` date DEFAULT NULL,
   `taskstatus` varchar(80) DEFAULT NULL,
-  `hourstracked` int(11) DEFAULT NULL,
-  `starttime` date DEFAULT NULL,
-  `endtime` date DEFAULT NULL,
+  `hourstracked` decimal(8,2) NOT NULL DEFAULT 0.00,
+  `starttime` datetime DEFAULT NULL,
+  `endtime` datetime DEFAULT NULL,
   `userid` varchar(50) DEFAULT NULL,
   `orgid` varchar(40) DEFAULT NULL,
+  `inprogress` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`taskid`),
   KEY `userid` (`userid`),
   KEY `fk_orgid` (`orgid`),
@@ -153,7 +213,7 @@ CREATE TABLE `task` (
 
 LOCK TABLES `task` WRITE;
 /*!40000 ALTER TABLE `task` DISABLE KEYS */;
-INSERT INTO `task` VALUES ('cU8NaLDYk9','Develop Login Module','Wjs51yYrSh','2024-10-12 10:00:00','2024-10-19','Open',12,'2024-10-12','2024-10-12','o8mLTxt1pg','eGpZhbaA38'),('E2prAOz6mD','Test Task1','Wjs51yYrSh','2024-11-07 17:50:31.672000','2024-11-30','Open',0,'0000-00-00','0000-00-00','Wjs51yYrSh','eGpZhbaA38');
+INSERT INTO `task` VALUES ('cU8NaLDYk9','Develop Login Module','Wjs51yYrSh','2024-10-12 10:00:00','2024-10-19','Completed',12.79,'2024-11-10 08:39:33','2024-11-10 09:23:55','o8mLTxt1pg','eGpZhbaA38',0),('E2prAOz6mD','Test Task1','Wjs51yYrSh','2024-11-07 17:50:31.672000','2024-11-30','Open',0.00,'0000-00-00 00:00:00','0000-00-00 00:00:00','Wjs51yYrSh','eGpZhbaA38',0),('FjptRVomBf','Test Task Email','Wjs51yYrSh','2024-11-11 12:13:15.836000','2024-11-22','Open',0.00,'0000-00-00 00:00:00','0000-00-00 00:00:00','qRmflSIf3A','eGpZhbaA38',NULL),('PaE74IJHv0','Today Task','Wjs51yYrSh','2024-11-08 17:52:02.470000','2024-11-08','Open',0.00,'0000-00-00 00:00:00','0000-00-00 00:00:00','o8mLTxt1pg','eGpZhbaA38',0),('pERFjG9Rm1','frontend ','Wjs51yYrSh','2024-11-11 14:08:22.268000','2024-11-30','Completed',0.00,'2024-11-11 08:42:24','2024-11-11 08:42:28','K1CehiENBO','eGpZhbaA38',0),('PkSNJzCg7W','Tomorrow Task','Wjs51yYrSh','2024-11-08 17:51:00.809000','2024-11-09','In Progress',0.68,'2024-11-10 08:36:33','2024-11-10 09:12:55','o8mLTxt1pg','eGpZhbaA38',0);
 /*!40000 ALTER TABLE `task` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -187,7 +247,7 @@ CREATE TABLE `teams` (
 
 LOCK TABLES `teams` WRITE;
 /*!40000 ALTER TABLE `teams` DISABLE KEYS */;
-INSERT INTO `teams` VALUES ('2K7wS04oIQ','Frontend team','Angular team','[\"o8mLTxt1pg\"]','2024-11-07','Wjs51yYrSh','Wjs51yYrSh','eGpZhbaA38'),('6YRvQilMG7','UI UX','Designers','[\"IpuJD6KF7D\"]','2024-10-24','3LqPQ1PAfH','3LqPQ1PAfH','MmffMGHkvb'),('aghhttUL6Q','Backend Developer','test developer','[\"3LqPQ1PAfH\"]','2024-10-24','3LqPQ1PAfH','3LqPQ1PAfH','MmffMGHkvb'),('aLz0hWKpMT','Production Team','List of all members of production team','[\"3LqPQ1PAfH\",\"IpuJD6KF7D\"]','2024-10-24','3LqPQ1PAfH','3LqPQ1PAfH','MmffMGHkvb'),('lko1KkIXq1','Test Tea,','test team','[\"IpuJD6KF7D\"]','2024-10-24','3LqPQ1PAfH','3LqPQ1PAfH','MmffMGHkvb'),('OTDGoOATPv','Frontend Developers','List of developers','[\"3LqPQ1PAfH\",\"IpuJD6KF7D\"]','2024-10-24','3LqPQ1PAfH','3LqPQ1PAfH','MmffMGHkvb');
+INSERT INTO `teams` VALUES ('2K7wS04oIQ','Frontend team','Angular team','[\"o8mLTxt1pg\"]','2024-11-07','Wjs51yYrSh','Wjs51yYrSh','eGpZhbaA38'),('6YRvQilMG7','UI UX','Designers','[\"IpuJD6KF7D\"]','2024-10-24','3LqPQ1PAfH','3LqPQ1PAfH','MmffMGHkvb'),('aghhttUL6Q','Backend Developer','test developer','[\"3LqPQ1PAfH\"]','2024-10-24','3LqPQ1PAfH','3LqPQ1PAfH','MmffMGHkvb'),('aLz0hWKpMT','Production Team','List of all members of production team','[\"3LqPQ1PAfH\",\"IpuJD6KF7D\"]','2024-10-24','3LqPQ1PAfH','3LqPQ1PAfH','MmffMGHkvb'),('lko1KkIXq1','Test Tea,','test team','[\"IpuJD6KF7D\"]','2024-10-24','3LqPQ1PAfH','3LqPQ1PAfH','MmffMGHkvb'),('OTDGoOATPv','Frontend Developers','List of developers','[\"3LqPQ1PAfH\",\"IpuJD6KF7D\"]','2024-10-24','3LqPQ1PAfH','3LqPQ1PAfH','MmffMGHkvb'),('sUNWKv5DQe','frontend team','html css','[\"K1CehiENBO\",\"o8mLTxt1pg\",\"qRmflSIf3A\"]','2024-11-11','K1CehiENBO','K1CehiENBO','eGpZhbaA38');
 /*!40000 ALTER TABLE `teams` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -227,7 +287,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('3LqPQ1PAfH','Shayban Khan','shaybankhan12345@gmail.com','Test123@','8149863141','camp','male','1999-10-23','2024-10-24','2024-10-24',101,'MmffMGHkvb','Active'),('IpuJD6KF7D','shayban test','lewage2024@aleitar.com','test123@',NULL,'camp',NULL,'9999-01-21','2024-10-24','2024-10-24',103,'MmffMGHkvb','active'),('o8mLTxt1pg','Test Employee','yobegey752@anypng.com','VGVzdDEyM0A=',NULL,'camp',NULL,'2024-11-07','2024-11-07','2024-11-07',103,'eGpZhbaA38','active'),('u101','shamsiya','sehrozkhan2704@gmail.com','Shams123@','7841849749','kharadi','female','0000-00-00','0000-00-00','0000-00-00',101,'1',NULL),('Wjs51yYrSh','Shayban Khan','organization@gmail.com','VGVzdDEyM0A=','9876543210','camp','male','2024-11-06','2024-11-06','2024-11-06',101,'eGpZhbaA38','Active');
+INSERT INTO `user` VALUES ('3LqPQ1PAfH','Shayban Khan','shaybankhan12345@gmail.com','Test123@','8149863141','camp','male','1999-10-23','2024-10-24','2024-10-24',101,'MmffMGHkvb','Active'),('IpuJD6KF7D','shayban test','lewage2024@aleitar.com','test123@',NULL,'camp',NULL,'9999-01-21','2024-10-24','2024-10-24',103,'MmffMGHkvb','active'),('K1CehiENBO','Shamsiya Khan','jibale6332@edectus.com','VGVzdDEyM0A=','8976543210','kharadi',NULL,'1999-04-27','2024-11-10','2024-11-10',102,'eGpZhbaA38','active'),('o8mLTxt1pg','Test Employee','yobegey752@anypng.com','VGVzdDEyM0A=',NULL,'camp',NULL,'2024-11-07','2024-11-07','2024-11-07',103,'eGpZhbaA38','active'),('qn0AMocI5V',NULL,'khan2704@gmail.com',NULL,NULL,NULL,NULL,NULL,'2024-11-11','2024-11-11',102,'eGpZhbaA38','inActive'),('qRmflSIf3A','test shayban','ririwag192@inikale.com','VGVzdDEyM0A=','1234567890','Camp',NULL,'2024-11-04','2024-11-11','2024-11-11',102,'eGpZhbaA38','active'),('u101','shamsiya','sehrozkhan2704@gmail.com','Shams123@','7841849749','kharadi','female','0000-00-00','0000-00-00','0000-00-00',101,'1',NULL),('Wjs51yYrSh','Shayban Khan','organization@gmail.com','VGVzdDEyM0A=','9876543210','camp','male','2024-11-06','2024-11-06','2024-11-06',101,'eGpZhbaA38','Active');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -248,4 +308,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-07 23:17:32
+-- Dump completed on 2024-11-11 14:45:05
